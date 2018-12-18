f334d4bac6ddaff133f2797d8673993b5da2a1ee
Allahbin <20111001xyz>
2018-12-19 00:38:26 +0800

    暂存晓晔的代码

--- aaa.cpp
+++ aaa.cpp
@@ -1,24 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    int I, m, J, sum;    
+    int i, I, n, m, b, J, sum;
+    I = 1;
+    J = 1;
     sum = 0;
     printf("Input the m:");
     scanf("%d", &m);
-    for (int n = 1; n <= m; n++)
+    for (n = 1; n <= m; n++)
     {
-        I = 0;
-        // 计算阶乘之和 1!+2!
-        for (int i = 1; i <= n; i++)
+        for (i = 1; i <= n; i++)
         {
-            // 初始化用于求和的J
-            J = 1;
-            // 计算阶乘 2! 之类的
-            for (int b = 1; b <= i; b++)
+
+            I = I * i;
+            for (b = 1; b < i; b++)
             {
                 J = J * b;
             }
-            // 将求出来的 2! 加上去 1!+2!
             I += J;
         }
         sum += I;
