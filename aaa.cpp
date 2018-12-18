6e57c94598a9287273c064bc31e200365ca2a6f6
Allahbin <20111001xyz>
2018-12-19 00:50:52 +0800

    修改完

--- aaa.cpp
+++ aaa.cpp
@@ -2,18 +2,16 @@
 int main()
 {
     int i, I, n, m, b, J, sum;
-    I = 1;
-    J = 1;
     sum = 0;
     printf("Input the m:");
     scanf("%d", &m);
     for (n = 1; n <= m; n++)
     {
+        I = 0;
         for (i = 1; i <= n; i++)
-        {
-
-            I = I * i;
-            for (b = 1; b < i; b++)
+        {         
+            J = 1;   
+            for (b = 1; b <= i; b++)
             {
                 J = J * b;
             }
